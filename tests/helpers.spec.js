3f4af694e3359dc9d067fff256c723cbfffd639d
Lajos Meszaros <m_lajos@hotmail.com>
2019-03-14 14:03:26 +0100

    test(helpers): added tests for isCent

--- tests/helpers.spec.js
+++ tests/helpers.spec.js
@@ -1,9 +1,24 @@
-describe('test', () => {
-  it('should pass', () => {
-    expect(1).toBeGreaterThan(0)
-  })
-
-  it('should not pass', () => {
-    expect(1).toBeGreaterThan(10)
+describe(`helpers`, () => {
+  describe(`isCent()`, () => {
+    it(`returns true, when given string is a floating point number`, () => {
+      expect(isCent('127.052')).toBe(true)
+    })
+    it(`returns true, when given string has a dot, but doesn't have any decimals written`, () => {
+      expect(isCent('150.')).toBe(true)
+    })
+    it(`returns false, when given string contains an integer`, () => {
+      expect(isCent('500')).toBe(false)
+    })
+    it(`returns true when given string contains whitespace around the number`, () => {
+      expect(isCent('700.0     ')).toBe(true)
+      expect(isCent('         700.0')).toBe(true)
+      expect(isCent('      700.0   \t')).toBe(true)
+    })
+    it(`returns false, when given string contains multiple numbers`, () => {
+      expect(isCent('700. 500.')).toBe(false)
+    })
+    it(`returns false, when the float in the given string contains whitespaces`, () => {
+      expect(isCent('3.   141592')).toBe(false)
+    })
   })
 })
