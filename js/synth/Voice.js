644cf2db29232fdc099c76b5cc1cfd0bef96f1ac
Lajos Meszaros <m_lajos@hotmail.com>
2019-04-15 10:23:02 +0200

    fix(Voice): added envelope interpolation for Firefox

--- js/synth/Voice.js
+++ js/synth/Voice.js
@@ -24,6 +24,60 @@ const getEnvelopeByName = name => {
 
 const getEnvelopeName = () => jQuery( '#input_select_synth_amp_env' ).val()
 
+// https://github.com/mohayonao/pseudo-audio-param/blob/master/lib/expr.js#L3
+function getLinearRampToValueAtTime(t, v0, v1, t0, t1) {
+  var a;
+
+  if (t <= t0) {
+    return v0;
+  }
+  if (t1 <= t) {
+    return v1;
+  }
+
+  a = (t - t0) / (t1 - t0);
+
+  return v0 + a * (v1 - v0);
+}
+
+// https://github.com/mohayonao/pseudo-audio-param/blob/master/lib/expr.js#L18
+function getExponentialRampToValueAtTime(t, v0, v1, t0, t1) {
+  var a;
+
+  if (t <= t0) {
+    return v0;
+  }
+  if (t1 <= t) {
+    return v1;
+  }
+  if (v0 === v1) {
+    return v0;
+  }
+
+  a = (t - t0) / (t1 - t0);
+
+  if ((0 < v0 && 0 < v1) || (v0 < 0 && v1 < 0)) {
+    return v0 * Math.pow(v1 / v0, a);
+  }
+
+  return 0;
+}
+
+const interpolateValueAtTime = (minValue, maxValue, envelope, t) => {
+  // interpolate attack
+  if (envelope.attackTime > t) {
+    return getLinearRampToValueAtTime(t, minValue, maxValue, 0, envelope.attackTime)
+  }
+
+  // interpolate decay
+  if (envelope.attackTime + envelope.decayTime > t) {
+    return getExponentialRampToValueAtTime(t, maxValue, maxValue * envelope.sustain, envelope.attackTime, envelope.attackTime + envelope.decayTime)
+  }
+
+  // interpolate sustain
+  return maxValue * envelope.sustain
+}
+
 class Voice {
   constructor (audioCtx, frequency, velocity) {
     this.frequency = frequency;
@@ -62,6 +116,7 @@ class Voice {
     this.vco.frequency.value = this.frequency;
   
     /* VCA */
+    this.vca.gain._startTime = now
     this.vca.gain.value = 0;
     this.vca.gain.setValueAtTime(this.vca.gain.value, now); // initial gain
     this.vca.gain.linearRampToValueAtTime(this.velocity, now + this.attackTime); // attack
@@ -82,11 +137,12 @@ class Voice {
     const now = this.synth.now();
     const vcaGain = this.vca.gain
     this.oscillators.forEach(oscillator => {
-      // Firefox doesn't support cancelAndHoldAtTime.. shame!!
+      // Firefox and Safari doesn't support cancelAndHoldAtTime.. shame!!
       if (isFunction(vcaGain.cancelAndHoldAtTime)) {
         vcaGain.cancelAndHoldAtTime(now);
       } else {
         vcaGain.cancelScheduledValues(now);
+        vcaGain.setValueAtTime(interpolateValueAtTime(0, this.velocity, getEnvelopeByName(getEnvelopeName()), now - this.vca.gain._startTime), now)
       }
       vcaGain.exponentialRampToValueAtTime( 0.001, now + this.releaseTime ); // release
       oscillator.stop( now + this.releaseTime );
