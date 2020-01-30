3ab759916848bd8bc53478df75b62591da59e9bd
vsicurella <sicurella12@gmail.com>
2020-01-30 18:39:40 -0500

    Deleted debug condition

--- js/modifiers.js
+++ js/modifiers.js
@@ -447,8 +447,6 @@ function modify_update_approximations() {
                 break;
             } else if ((centsdabs >= mincentsd && centsdabs <= maxcentsd) && (prime_limit >= minprime && prime_limit <= maxprime)) {
                 $("#approximation_selection").append("<option>"+description+"</option>");
-            } else {
-                //debug("Option excluded: " + description);
             }
         }
                 
