423b7add268ff470000d054dc1bf8d31742dbb5b
Sean Archibald <sean@sevish.com>
2020-12-22 21:38:59 +0000

    extra newline before [Scale End] in tun export

--- src/js/exporters.js
+++ src/js/exporters.js
@@ -82,7 +82,7 @@ function export_anamark_tun(version) {
     }
   
     file += newline + "; Set reference key to absolute frequency (not scale note but midi key)" + newline;
-    file += "note " + tuning_table['base_midi_note'] + '="! ' + tuning_table['base_frequency'].toFixed(6) + '"' + newline;
+    file += "note " + tuning_table['base_midi_note'] + '="! ' + tuning_table['base_frequency'].toFixed(6) + '"' + newline + newline;
 
   }
 
