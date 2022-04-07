3ce7062b82535fc7e298c3c88e353d3bfc6db0d4
Lajos Meszaros <m_lajos@hotmail.com>
2022-04-08 00:30:55 +0200

    added port id to the midi device names, because same devices would clash

--- src/js/midi/helpers.js
+++ src/js/midi/helpers.js
@@ -1,4 +1,6 @@
-const getNameFromPort = (port) => `${port.name} (version ${port.version}) ${port.manufacturer}`
+const getNameFromPort = (port) => {
+  return `${port.name} (version ${port.version}) ${port.manufacturer} (${port.id})`
+}
 
 const getAllKeys = () => R.unfold((n) => (n > keyIdMax ? false : [n, n + 1]), keyIdMin)
 const getWhiteKeys = R.compose(R.map(parseInt), R.keys, () => whiteOnlyMap)
