edfeba741f7d86e4782b37f07ef1e2a04ac34d81
Lajos Meszaros <m_lajos@hotmail.com>
2022-04-23 17:21:17 +0200

    made Decimal function calls inline

--- src/js/midi/math.js
+++ src/js/midi/math.js
@@ -1,25 +1,11 @@
-const add = (a, b) => Decimal.add(a, b)
-
-const subtract = (a, b) => Decimal.sub(a, b)
-
-const multiply = (a, b) => Decimal.mul(a, b)
-
-const divide = (a, b) => Decimal.div(a, b)
-
-const pow = (a, b) => Decimal.pow(a, b)
-
-const log = (a, b) => Decimal.log(a, b)
-
-const floor = (a) => Decimal.floor(a)
-
 const moveNUnits = (ratioOfSymmetry, divisionsPerRatio, n, frequency) => {
   // return frequency * ratioOfSymmetry ** (n / divisionsPerRatio)
-  return multiply(frequency, pow(ratioOfSymmetry, divide(n, divisionsPerRatio)))
+  return Decimal.mul(frequency, Decimal.pow(ratioOfSymmetry, Decimal.div(n, divisionsPerRatio)))
 }
 
 const getDistanceInUnits = (ratioOfSymmetry, divisionsPerRatio, freq2, freq1) => {
   // return divisionsPerRatio * Math.log(freq2 / freq1, ratioOfSymmetry)
-  return multiply(divisionsPerRatio, log(divide(freq2, freq1), ratioOfSymmetry))
+  return Decimal.mul(divisionsPerRatio, Decimal.log(Decimal.div(freq2, freq1), ratioOfSymmetry))
 }
 
 const moveNSemitones = (n, frequency) => {
@@ -38,11 +24,13 @@ const getBendingDistance = (freq2, freq1) => {
 
 const getNoteFrequency = (midinote) => {
   return moveNSemitones(
-    subtract(R.clamp(0, 127, midinote), referenceNote.id),
+    Decimal.sub(R.clamp(0, 127, midinote), referenceNote.id),
     referenceNote.frequency
   )
 }
 
 const getNoteId = (frequency) => {
-  return floor(add(getDistanceInSemitones(frequency, referenceNote.frequency), referenceNote.id))
+  return Decimal.floor(
+    Decimal.add(getDistanceInSemitones(frequency, referenceNote.frequency), referenceNote.id)
+  )
 }
